9d5c86589fb8acd540d9fbaff5efded7f6a74679
ormazabal <ormazabalnascimento@gmail.com>
2023-04-23 21:59:10 -0300

    opcao local e remoto

--- Program.cs
+++ Program.cs
@@ -8,14 +8,19 @@ namespace iptv_cleaner
     {
         public static void Main()
         {
-            string[] quality = {"FHD","4K"};
+            string[] quality = {"FHD¹","FHD"};
             string line, substr;
-	        string list_in = "";
-            bool next = false;
-            WebClient client = new WebClient();
-            Stream stream = client.OpenRead(list_in);
-            StreamReader sr = new StreamReader(stream);
+			bool next = false;
+			
+	        //string list_in = "";
+            //WebClient client = new WebClient();
+            //Stream stream = client.OpenRead(list_in);
+            //StreamReader sr = new StreamReader(stream);
+			
+			StreamReader sr = new StreamReader("list-in.m3u");
             StreamWriter sw = new StreamWriter("list-out.m3u");
+			sw.WriteLine("#EXTM3U x-tvg-url=\"http://u.mmds.fun/epg\"");
+			
             while ((line = sr.ReadLine()) != null)
             {
                 if(next) 
@@ -26,10 +31,11 @@ namespace iptv_cleaner
                 }
                 foreach (string item in quality)
                 {
-                    substr = line.Substring(line.Length-item.Length-1, item.Length);
+                    substr = line.Substring(line.Length-item.Length, item.Length);
                     if(substr == item)
                     {
-                        sw.WriteLine(line);
+						substr = line.Replace(" " + substr, "");
+                        sw.WriteLine(substr.Trim());
                         next = true;
                         break;
                     }
